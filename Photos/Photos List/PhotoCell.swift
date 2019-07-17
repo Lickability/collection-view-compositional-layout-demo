ed028c8e4f01b7c38798e654a328cd1d01704b9e
Michael Liberatore <mi@chael.co>
2019-07-17 16:27:38 -0400

    Adds basic photo cell

---
+++ Photos/Photos List/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  Photos
+//
+//  Created by Michael Liberatore on 7/17/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import UIKit
+
+final class PhotoCell: UICollectionViewCell {
+    @IBOutlet private weak var imageView: UIImageView!
+    
+}
